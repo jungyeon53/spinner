942a0216ffde976ca348665fd441e626ef248ffa
jungyeon <wjddus5533@naver.com>
2024-10-07 22:01:15 +0900

    Add all project files

---
+++ src/main/java/com/spinner/www/SpinnerApplication.java
@@ -0,0 +1,14 @@
+package com.spinner.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpinnerApplication.class, args);
+    }
+    //
+
+}
